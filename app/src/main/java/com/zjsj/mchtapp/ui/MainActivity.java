d3f7c1690b3360d94943826da60da68a5d019941
wanruome <wanruome@163.com>
2018-06-26 17:56:33 +0800

    主页设置。

--- app/src/main/java/com/zjsj/mchtapp/ui/MainActivity.java
+++ app/src/main/java/com/zjsj/mchtapp/ui/MainActivity.java
@@ -2,14 +2,51 @@ package com.zjsj.mchtapp.ui;
 
 import android.os.Bundle;
 
+import com.ruomm.base.ioc.activity.BaseFragmentActivity;
+import com.ruomm.base.ioc.annotation.view.InjectAll;
+import com.ruomm.base.ioc.annotation.view.InjectView;
+import com.ruomm.base.ioc.iocutil.BaseUtil;
+import com.ruomm.base.view.slidingmenulib.SlidingMenu;
 import com.zjsj.mchtapp.R;
 import com.zjsj.mchtapp.config.ui.BaseAppActivity;
 
-public class MainActivity extends BaseAppActivity{
+public class MainActivity extends BaseFragmentActivity{
+    @InjectAll
+    Views views = new Views();
 
+    class Views {
+        @InjectView(id = R.id.slidingmenumain)
+        SlidingMenu mySlidingMenu;
+
+    }
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
-        setInitContentView(R.layout.welcome_act);
+        setContentView(R.layout.slidingmenumain);
+        BaseUtil.initInjectAll(this);
+        // 设置侧滑菜单的类型
+        views.mySlidingMenu.setMode(SlidingMenu.LEFT);
+        // 设置触摸屏幕的模式
+        views.mySlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        views.mySlidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+
+        views.mySlidingMenu.setShadowDrawable(R.drawable.slidingmenu_bg);
+
+        // 设置滑动菜单视图的宽度
+        views.mySlidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        // 设置渐入渐出效果的值
+        views.mySlidingMenu.setFadeDegree(0.35f);
+        /**
+         * SLIDING_WINDOW will include the Title/ActionBar in the content section of the
+         * SlidingMenu, while SLIDING_CONTENT does not.
+         */
+        // views.mySlidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        // 为侧滑菜单设置布局
+        // views.mySlidingMenu.setMenu(R.layout.fragment_menu);
+
+        views.mySlidingMenu.setMenu(R.layout.main_menu_lay);
+        views.mySlidingMenu.setContent(R.layout.main_homepage_lay);
+        views.mySlidingMenu.getBehindOffset();
+
     }
 }
--- app/src/main/java/com/zjsj/mchtapp/ui/WelcomeActivity.java
+++ app/src/main/java/com/zjsj/mchtapp/ui/WelcomeActivity.java
@@ -1,16 +1,35 @@
 package com.zjsj.mchtapp.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.ruomm.base.ioc.annotation.view.InjectView;
+import com.ruomm.base.tools.ToastUtil;
 import com.zjsj.mchtapp.R;
 import com.zjsj.mchtapp.config.ui.BaseAppActivity;
 
 public class WelcomeActivity extends BaseAppActivity
 {
+    @InjectView(id=R.id.view_img)
+    private ImageView view_img;
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         setInitContentView(R.layout.welcome_act);
         hideMenuTopView();
+        view_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.makeFailToastThr(mContext,"测试一下");
+                startActivity(new Intent(mContext,MainActivity.class));
+            }
+        });
+    }
+    public void myOnClick(View view)
+    {
+        ToastUtil.makeFailToastThr(mContext,"测试一下");
+        startActivity(new Intent("mchtapp.mainActivity"));
     }
 }
