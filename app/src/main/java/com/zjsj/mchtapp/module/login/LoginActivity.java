be1534d652b575476b0e245e27d0b2d0023700ef
wanruome <wanruome@163.com>
2018-07-05 21:25:34 +0800

    自定义键盘。

--- app/src/main/java/com/zjsj/mchtapp/module/login/LoginActivity.java
+++ app/src/main/java/com/zjsj/mchtapp/module/login/LoginActivity.java
@@ -3,8 +3,10 @@ package com.zjsj.mchtapp.module.login;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -17,6 +19,8 @@ import com.ruomm.baseconfig.debug.MLog;
 import com.ruomm.resource.ui.AppMultiActivity;
 import com.zjsj.mchtapp.R;
 import com.zjsj.mchtapp.util.keyboard.KeyboardUtil;
+
+import java.lang.reflect.Method;
 
 public class LoginActivity extends AppMultiActivity {
     @InjectAll
@@ -38,34 +42,13 @@ public class LoginActivity extends AppMultiActivity {
         Button btn_submit;
         @InjectView(id=R.id.ly_fast_register)
         LinearLayout ly_fast_register;
-
     }
+    private KeyboardUtil keyboardUtil;
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         hideMenuTopView();
         setInitContentView(R.layout.login_act);
-        views.edt_pwd.setInputType(InputType.TYPE_NULL);
-        views.edt_pwd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                KeyboardUtil.shared(LoginActivity.this,views.edt_pwd).showKeyboard();
-            }
-        });
-//       ViewGroup viewGroup= (ViewGroup) findViewById(android.R.id.content);
-//       int i=viewGroup.getChildCount();
-//        MLog.i(i);
-//        ViewGroup rootView = (ViewGroup) ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
-//
-//        View view= LayoutInflater.from(this).inflate(R.layout.main_menu_lay,null);
-//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-//        layoutParams.addRule(RelativeLayout.ALIGN_BOTTOM, RelativeLayout.TRUE);
-//        viewGroup.addView(view,layoutParams);
-//        views.btn_submit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
+//        keyboardUtil=new KeyboardUtil(this,views.edt_pwd);
     }
 }
--- app/src/main/java/com/zjsj/mchtapp/util/keyboard/KeyboardUtil.java
+++ app/src/main/java/com/zjsj/mchtapp/util/keyboard/KeyboardUtil.java
@@ -4,15 +4,19 @@ import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
 import android.text.Editable;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 import com.ruomm.base.tools.DisplayUtil;
 import com.zjsj.mchtapp.R;
+import com.zjsj.mchtapp.module.login.LoginActivity;
+
+import java.lang.reflect.Method;
 
 public class KeyboardUtil {
 
@@ -40,33 +44,101 @@ public class KeyboardUtil {
 
     private ViewGroup rootView;
     private View keyboradContainer;
+
+    private View kv_img_hidden;
+    private View kv_lyt_container;
     private KeyboardView keyboardView;
-    private EditText ed;
     private LayoutInflater  mLayoutInflater;
+    private EditText mEditText;
+    private Activity mActivity;
 
 
 
-    private KeyboardUtil(Activity activity, EditText edit) {
-        this.ed = edit;
+    public KeyboardUtil(Activity activity, EditText edit) {
+        this.mEditText = edit;
+        mActivity=activity;
         k1 = new Keyboard(activity, R.xml.key_letter);
         k2 = new Keyboard(activity,R.xml.key_number);
         k3 = new Keyboard(activity,R.xml.key_symbol);
         mLayoutInflater=LayoutInflater.from(activity);
-
 //        keyboardView = new KeyboardView(activity, null);
         rootView = (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content));
         keyboradContainer=mLayoutInflater.inflate(R.layout.lyt_keyboard,null);
+
+//        int height= DisplayUtil.getNavigationBarHeight(activity);
+//        if(height>0)
+//        {
+//           keyboradContainer.setPadding(0,0,0,height);
+//        }
+        kv_img_hidden=keyboradContainer.findViewById(R.id.kv_img_hidden);
+        kv_lyt_container=keyboradContainer.findViewById(R.id.kv_lyt_container);
+        keyboardView = keyboradContainer.findViewById(R.id.kv_lyt_keyboard);
         int height= DisplayUtil.getNavigationBarHeight(activity);
         if(height>0)
         {
-           keyboradContainer.setPadding(0,0,0,height);
+            kv_lyt_container.setPadding(0,mActivity.getResources().getDimensionPixelSize(R.dimen.dpx006),0,height);
+            kv_lyt_container.setPadding(0,mActivity.getResources().getDimensionPixelSize(R.dimen.dpx006),0,mActivity.getResources().getDimensionPixelSize(R.dimen.dpx032));
         }
-        keyboardView = keyboradContainer.findViewById(R.id.kv_lyt_keyboard);
-        keyboardView.setKeyboard(k3);
+        else {
+            kv_lyt_container.setPadding(0,mActivity.getResources().getDimensionPixelSize(R.dimen.dpx006),0,mActivity.getResources().getDimensionPixelSize(R.dimen.dpx080));
+        }
+        keyboardView.setKeyboard(k1);
         keyboardView.setEnabled(true);
         keyboardView.setPreviewEnabled(false);
         keyboardView.setOnKeyboardActionListener(onKeyboardActionListener);
+        kv_img_hidden.setOnClickListener(kvOnClickListener);
+        mEditText.setOnTouchListener(kvOnTouchListener);
+        mEditText.setOnFocusChangeListener(kvOnFocusChangeListener);
     }
+    private View.OnClickListener kvOnClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            hideKeyboard();
+        }
+    };
+    private View.OnFocusChangeListener kvOnFocusChangeListener=new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    hideKeyboard();
+                }
+        }
+    };
+    private View.OnTouchListener kvOnTouchListener=new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if(isShow)
+            {
+                return false;
+            }
+            int eventAction=event.getAction();
+            if(eventAction==MotionEvent.ACTION_UP) {
+
+                int sdk_int = android.os.Build.VERSION.SDK_INT;
+                if (sdk_int <= 10) {
+                    mEditText.setInputType(InputType.TYPE_NULL);
+                } else {
+                    mActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+                    try {
+                        Class<EditText> cls = EditText.class;
+                        Method setSoftInputShownOnFocus;
+                        setSoftInputShownOnFocus = cls.getMethod("setShowSoftInputOnFocus", boolean.class);
+                        setSoftInputShownOnFocus.setAccessible(true);
+                        setSoftInputShownOnFocus.invoke(mEditText, false);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                showKeyboard();
+                return false;
+            }
+            else{
+                return false;
+            }
+        }
+    };
 
     OnKeyboardActionListener onKeyboardActionListener = new OnKeyboardActionListener() {
         @Override
@@ -99,9 +171,9 @@ public class KeyboardUtil {
 
         @Override
         public void onKey(int primaryCode, int[] keyCodes) {
-            Editable editable = ed.getText();
-            int start = ed.getSelectionStart();
-            if (primaryCode == Keyboard.KEYCODE_CANCEL) {// 完成
+            Editable editable = mEditText.getText();
+            int start = mEditText.getSelectionStart();
+            if (primaryCode ==ACTION_DONE) {// 完成
                 hideKeyboard();
             } else if (primaryCode == ACTION_DELETE) {// 回退
                 if (editable != null && editable.length() > 0) {
@@ -126,13 +198,26 @@ public class KeyboardUtil {
                 keyboardView.setKeyboard(k3);
             } else if (primaryCode == ACTION_LETF) { //向左
                 if (start > 0) {
-                    ed.setSelection(start - 1);
+                    mEditText.setSelection(start - 1);
                 }
             } else if (primaryCode == ACTION_RIGHT) { // 向右
-                if (start < ed.length()) {
-                    ed.setSelection(start + 1);
+                if (start < mEditText.length()) {
+                    mEditText.setSelection(start + 1);
                 }
-            } else {
+            }
+            else if(primaryCode==MONEY_RMB)
+            {
+                editable.insert(start,"¥");
+            }
+            else if(primaryCode==MONEY_OY)
+            {
+                editable.insert(start,"€");
+            }
+            else if(primaryCode==MONEY_YB)
+            {
+                editable.insert(start,"£");
+            }
+            else {
                 String str = Character.toString((char) primaryCode);
                 if (isWord(str)) {
                     if (isUpper) {
@@ -153,28 +238,39 @@ public class KeyboardUtil {
         if (!isShow) {
             rootView.addView(keyboradContainer);
             isShow = true;
+            DisplayUtil.closeSoftInputView(mActivity);
         }
     }
 
-    private void hideKeyboard() {
+    public void hideKeyboard() {
         if (rootView != null && keyboardView != null && isShow) {
             isShow = false;
             rootView.removeView(keyboradContainer);
+
         }
-        mInstance = null;
+//        mInstance = null;
+    }
+    private void destoryKeyBoard(){
+        hideKeyboard();
+        this.keyboradContainer=null;
+        this.keyboardView=null;
+        this.mEditText.setOnTouchListener(null);
+        this.mEditText.setOnFocusChangeListener(null);
+        this.mActivity=null;
+        this.mEditText=null;
     }
 
     private boolean isWord(String str) {
         return str.matches("[a-zA-Z]");
     }
-
-    private static KeyboardUtil mInstance;
-
-    public static KeyboardUtil shared(Activity activity, EditText edit) {
-        if (mInstance == null) {
-            mInstance = new KeyboardUtil(activity, edit);
-        }
-        mInstance.ed = edit;
-        return mInstance;
-    }
+//
+//    private static KeyboardUtil mInstance;
+//
+//    public static KeyboardUtil shared(Activity activity, EditText edit) {
+//        if (mInstance == null) {
+//            mInstance = new KeyboardUtil(activity, edit);
+//        }
+//        mInstance.mEditText = edit;
+//        return mInstance;
+//    }
 }
